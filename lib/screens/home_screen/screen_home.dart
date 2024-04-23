da1f3482231953fde55060cb2a59ecc0a4d213bb
Aflah Sedhique <aflah3100@gmail.com>
2024-04-23 10:41:16 +0530

    Devloped News Headlines Scroll Widget

--- lib/screens/home_screen/screen_home.dart
+++ lib/screens/home_screen/screen_home.dart
@@ -1,10 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feed/models/news_headlines_model/news_headlines_model.dart';
 import 'package:flutter_feed/services/apicalls.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
@@ -13,6 +13,7 @@ class ScreenHome extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height * 1;
     final width = MediaQuery.sizeOf(context).width * 1;
+    const dateFormat = 'dd MMMM, yyyy';
 
     return Scaffold(
       appBar: AppBar(
@@ -56,9 +57,12 @@ class ScreenHome extends StatelessWidget {
                         itemBuilder: (BuildContext context, int index) {
                           final currentArticle =
                               snapshot.data!.articles![index];
+                          final dateTime =
+                              DateTime.parse(currentArticle.publishedAt!);
 
                           return Stack(
                             children: [
+                              //News-Image-Widget
                               Container(
                                 height: height * 0.5,
                                 width: width * 0.9,
@@ -81,7 +85,74 @@ class ScreenHome extends StatelessWidget {
                                         const Icon(Icons.error),
                                   ),
                                 ),
-                              )
+                              ),
+                              //News-Headline-Widget
+                              Positioned(
+                                bottom: 0,
+                                left: height *
+                                    .02, // Adjust the left positioning to match the padding
+                                right: height *
+                                    .02, // Adjust the right positioning to match the padding
+                                child: Card(
+                                  elevation: 4,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  child: Container(
+                                    padding: EdgeInsets.only(
+                                        left: height * .02,
+                                        right: height * .02,
+                                        top: height * .015),
+                                    width: width * .9,
+                                    height: height * .2,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        //News-Heading
+                                        Center(
+                                            child: Text(
+                                          currentArticle.title!,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: GoogleFonts.poppins(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                        //News-Details
+                                        Padding(
+                                          padding: EdgeInsets.only(
+                                              bottom: height * .01),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(currentArticle.author!,
+                                                  style: GoogleFonts.poppins(
+                                                      fontSize: 15.0,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Colors.redAccent)),
+                                              Text(
+                                                DateFormat(dateFormat)
+                                                    .format(dateTime),
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 14.0,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ],
                           );
                         },
